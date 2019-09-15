bb6f64c332118d9966769908cf224471bce274f7
Daniel Kellmeyer <dsk96139@creighton.edu>
2019-09-15 16:04:31 -0500

    Updated: Final 1
    For Toms review

---
+++ forms/Q1/Q1.js
@@ -0,0 +1,4 @@
+let products = ['dog t-shirt',4.55,'cat t-shirt',6.77,'horse t-shirt',11.99]
+console.log(products[6])
+
+/* There would be no output since the range of the products is 0-5. */
---
+++ forms/Q2/Q2.js
@@ -0,0 +1,4 @@
+let numbers = [3,17,94,11,45,76,22,2,77,32,87]
+let length = numbers.length
+console.log(`There are ${length} numbers in the array.`)
+
